4d92e62a52031bfcee3862af7416f446c7a74741
Edwin Ortiz <edarcode@gmail.com>
2024-10-15 11:37:31 -0500

    agg seguidores y seguidos a una cuenta

--- src/modules/account/get-account/getAccountService.ts
+++ src/modules/account/get-account/getAccountService.ts
@@ -1,13 +1,41 @@
 import { db } from "../../../db/db";
 import { UUID } from "crypto";
 import { RecordNotFoundErr } from "../../../errors/RecordNotFoundErr";
+import { accounts, follows } from "../../../db/schemas";
+import { eq } from "drizzle-orm";
 
 export const getAccountService = async (accountId: UUID) => {
-  const account = await db.query.accounts.findFirst({
-    where: (accounts, { eq }) => eq(accounts.id, accountId),
-  });
+  const [account] = await db
+    .select()
+    .from(accounts)
+    .where(eq(accounts.id, accountId))
+    .limit(1);
 
   if (!account) throw new RecordNotFoundErr();
 
-  return account;
+  const [followingTo, followedBy] = await Promise.all([
+    db
+      .select({
+        id: accounts.id,
+        name: accounts.name,
+        img: accounts.img,
+        email: accounts.email,
+      })
+      .from(follows)
+      .where(eq(follows.followerId, accountId))
+      .innerJoin(accounts, eq(accounts.id, follows.followedId)),
+
+    db
+      .select({
+        id: accounts.id,
+        name: accounts.name,
+        img: accounts.img,
+        email: accounts.email,
+      })
+      .from(follows)
+      .where(eq(follows.followedId, accountId))
+      .innerJoin(accounts, eq(accounts.id, follows.followerId)),
+  ]);
+
+  return { ...account, followingTo, followedBy };
 };
