9a24c1330cca4e91a75a9a2e6dcd79fef10bc098
Edwin Ortiz <edarcode@gmail.com>
2024-10-16 20:12:02 -0500

    agg socket

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,31 @@
 import "./services/dotenv";
 import { PORT, SERVER_ON } from "./server/consts";
 import server from "./server/server";
+import { Server } from "socket.io";
+import http from "http";
+import { saveMsgService } from "./modules/messages/save-msg/saveMsgService";
+
+// socket.io
+
+const httpServer = http.createServer(server);
+const io = new Server(httpServer, { cors: { origin: process.env.CLIENT_URL } });
+
+io.on("connection", (socket) => {
+  console.log("clint connected", socket.id);
+
+  socket.on("joinRoom", ({ senderId, receiverId }) => {
+    const room = [senderId, receiverId].sort().join("-");
+    socket.join(room);
+  });
+
+  socket.on("message", async (msg) => {
+    const { senderId, receiverId, text } = msg;
+    const room = [senderId, receiverId].sort().join("-");
+    await saveMsgService(senderId, receiverId, { text });
+    io.to(room).emit("message", text);
+  });
+});
 
 // server-up
 
-server.listen(PORT, () => console.log(SERVER_ON));
+httpServer.listen(PORT, () => console.log(SERVER_ON));
--- src/modules/auth/verify-register/verifyRegisterController.ts
+++ src/modules/auth/verify-register/verifyRegisterController.ts
@@ -54,7 +54,7 @@ const page = `
         <body>
           <div>
             <span>¡Enhorabuena! Has verificado tu cuenta correctamente.</span>
-            <a href="${process.env.CLIENT_URL}">Ir a ...</a>
+            <a href="${process.env.CLIENT_URL}/login">Ir a ...</a>
           </div>
         </body>
       </html>
