89f9a6853df548554bbec61909d6d4d82acc0468
Edwin Ortiz <edarcode@gmail.com>
2024-10-16 22:18:55 -0500

    fix getChatService

--- src/modules/messages/get-chat/getChatService.ts
+++ src/modules/messages/get-chat/getChatService.ts
@@ -2,21 +2,23 @@ import { UUID } from "crypto";
 import { db } from "../../../db/db";
 
 export const getChatService = async (senderId: UUID, receiverId: UUID) => {
-  const msgSent = await db.query.messages.findMany({
-    where: (messages, { eq, and }) =>
-      and(eq(messages.senderId, senderId), eq(messages.receiverId, receiverId)),
+  const messages = await db.query.messages.findMany({
+    where: (messages, { eq, or, and }) =>
+      or(
+        and(
+          eq(messages.senderId, senderId),
+          eq(messages.receiverId, receiverId)
+        ),
+        and(
+          eq(messages.senderId, receiverId),
+          eq(messages.receiverId, senderId)
+        )
+      ),
     orderBy: (messages, { desc }) => [desc(messages.createdAt)],
-    limit: 8,
+    limit: 16,
   });
 
-  const msgReceived = await db.query.messages.findMany({
-    where: (messages, { eq, and }) =>
-      and(eq(messages.senderId, receiverId), eq(messages.receiverId, senderId)),
-    orderBy: (messages, { desc }) => [desc(messages.createdAt)],
-    limit: 8,
-  });
-
-  return [...msgSent, ...msgReceived].sort(
+  return messages.sort(
     (a, b) => new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime()
   );
 };
